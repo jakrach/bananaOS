9b27fd287697620340b9e2e8c47d13db62afbe72
Chris Rodriguez <chris.rodriguez1995@gmail.com>
2016-04-09 10:44:32 -0400

    Added a basic index.html placeholder file and a bananaOs.js file
    Index.html is a placeholder till we figure out how Node.js works, and
    bananaOs.js is going to be our main client-side JS file which will load
    and render our content, whether using Canvas or pure HTML5.

---
+++ bananaOs.js
@@ -0,0 +1,8 @@
+/**
+ * BananaOS main object file. Who knows what we're going to do in here.
+ * 
+ * @author Chris Rodriguez
+ */
+$(document).ready(function(){
+	console.log("Everything is loading. This is good.");
+});
