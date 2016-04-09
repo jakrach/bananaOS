5963ad35bc9195e85689f6e82b5dcea3614802d6
Chris Rodriguez <chris.rodriguez1995@gmail.com>
2016-04-09 14:55:30 -0400

    BananaOS Startup Tone + Loading Screen Updates
    Added BananaOS startup tone in MP3 and OGG formats
    Updates JS to fade out the loading text and fade in the bananaOS logo
    and play BananaOS startup tone.

--- public/js/bananaOs.js
+++ public/js/bananaOs.js
@@ -11,11 +11,11 @@ $(document).ready(function(){
 function showWord(word, speed, pos, callback){
 	if(pos < word.length){
 		window.setTimeout(function(word_n, speed_n, pos_n, callback){
-			$("body").append(word[pos_n]);
+			$("#loadingMessages").append(word[pos_n]);
 			showWord(word_n, speed_n, pos_n+1, callback);
 		}, speed, word, speed, pos, callback);
 	} else {
-		$("body").append("<br>");
+		$("#loadingMessages").append("<br>");
 		
 		if(typeof(callback) == "function"){
 			callback();
@@ -40,28 +40,13 @@ function BananaOS(){
 	                            ["Book<br>", 500, false],
 	                            ["Book<br>", 100, false],
 	                            ["Book<br>", 100, false],
-	                            ]
+	                            ];
 	
 	this.init = function(){
 		console.log("In bananaOS init function");
 		this.loadingMessageNumber = 0;
 		this.showLoadingMessagesN();
 	}
-	
-	/*this.showLoadingMessages = function(){
-		window.setTimeout(function(message, t_context, animateWord){
-			t_context.loadingMessageNumber++;
-			
-			if(animateWord){
-				showWord(message, 50, 0, function(){$(document).get(0).t_bananaOs.showLoadingMessages();});
-			} else {
-				$("body").append(message);
-				if(t_context.loadingMessageNumber < t_context.loadingMessageQueue.length){
-					t_context.showLoadingMessages();
-				}
-			}
-		}, this.loadingMessageQueue[this.loadingMessageNumber][1], this.loadingMessageQueue[this.loadingMessageNumber][0], this, this.loadingMessageQueue[this.loadingMessageNumber][2]);
-	}*/
 	
 	this.showLoadingMessagesN = function(){
 		if(this.loadingMessageNumber < this.loadingMessageQueue.length){
@@ -70,11 +55,20 @@ function BananaOS(){
 				if(message[2]){
 					showWord(message[0], message[3], 0, function(){$(document).get(0).t_bananaOs.showLoadingMessagesN();});
 				} else {
-					$("body").append(message[0]);
+					$("#loadingMessages").append(message[0]);
 					t_context.showLoadingMessagesN();
 				}
 			}, message[1], message, this);
+		} else {
+			$("#loadingMessages").fadeOut(1000, function(){
+				(new Audio("public/audio/bananaOsTone.ogg")).play();
+				$("#loadingBananaBoxHidingCont").fadeIn(1000);
+			});
 		}
+	}
+	
+	this.showBananaLoadingScreen = function(){
+		
 	}
 	
 	this.init();
